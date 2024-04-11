6b59af4d9bf835d010d53d047165e9d9c15755a1
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-11 17:30:20 -0500

    Estructura de directorios y primeras páginas.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,11 @@
 import { titleFont } from "@/config/fonts";
-import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
+    <div>
       <h1>Hola Mundo</h1>
       <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
       <h1 className={`${titleFont.className}`}>Hola Mundo</h1>
-    </main>
+    </div>
   );
 }
