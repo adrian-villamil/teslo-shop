fd1e43e5f3251582f40a82e071e7675636648c55
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-12 16:44:09 -0500

    Componente Title.

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,14 +1,16 @@
 import { TopMenu } from "@/components";
 
 export default function ShopLayout({
- children
+  children
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <main className="min-h-screen">
       <TopMenu />
-      {children}
+      <div className="px-0 sm:px-10">
+        {children}
+      </div>
     </main>
   );
 }
