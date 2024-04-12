fb5de2d415c4ff0283c3805390d7a6f862bf3ca0
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-12 18:37:47 -0500

    Tarjeta del producto.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { PageNotFound } from './ui/not-found/PageNotFound';
 export { ProductGrid } from './products/product-grid/ProductGrid';
+export { ProductGridItem } from './products/product-grid/ProductGridItem';
 export { Title } from './ui/title/Title';
 export { TopMenu } from './ui/top-menu/TopMenu';
