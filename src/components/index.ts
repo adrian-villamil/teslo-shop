7b4e86ab2e79669ae04767d2ae2a42e46c7a2aab
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-13 17:15:54 -0500

    Se agrega el sidebar al layout del home.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { PageNotFound } from './ui/not-found/PageNotFound';
 export { ProductGrid } from './products/product-grid/ProductGrid';
 export { ProductGridItem } from './products/product-grid/ProductGridItem';
+export { SideBar } from './ui/sidebar/SideBar';
 export { Title } from './ui/title/Title';
 export { TopMenu } from './ui/top-menu/TopMenu';
