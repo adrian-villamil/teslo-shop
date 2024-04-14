8856759993ca2d18e2df910f55c77bd37b4e108a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-13 23:31:59 -0500

    Slideshow de imágenes con thumbnails usando Swiper js.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export { PageNotFound } from './ui/not-found/PageNotFound';
 export { ProductGrid } from './products/product-grid/ProductGrid';
 export { ProductGridItem } from './products/product-grid/ProductGridItem';
+export { ProductSlideshow } from './product/slideshow/ProductSlideshow';
+export { QuantitySelector } from './product/quantity-selector/QuantitySelector';
 export { SideBar } from './ui/sidebar/SideBar';
 export { SizeSelector } from './product/size-selector/SizeSelector';
 export { Title } from './ui/title/Title';
 export { TopMenu } from './ui/top-menu/TopMenu';
-export { QuantitySelector } from './product/quantity-selector/QuantitySelector';
