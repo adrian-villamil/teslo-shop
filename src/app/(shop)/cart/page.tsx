244d9aef1dd7b6d7846552269d790aba3908ecd2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-16 21:06:05 -0500

    Pantalla del carrito de compras vacío.

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,8 @@
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 
 const productsInCart = [
   initialData.products[0],
@@ -10,6 +11,8 @@ const productsInCart = [
 ]
 
 export default function CartPage() {
+  // redirect('/empty');
+  
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
