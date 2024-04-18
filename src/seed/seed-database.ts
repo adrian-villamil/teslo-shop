d3a0ebb0fd5d425059c33255ad8b4801cf04b371
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-18 14:39:21 -0500

    Semilla: products.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -18,15 +18,26 @@ async function main() {
   });
 
   const categoriesDB = await prisma.category.findMany();
-  
+
   const categoriesMap = categoriesDB.reduce((map, category) => {
     map[category.name.toLowerCase()] = category.id;
     return map;
   }, {} as Record<string, string>);
 
   console.log(categoriesMap);
-  
+
   // Productos
+  products.forEach(async (product) => {
+    const {type,images, ...rest} = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    });
+  });
+
+  // Images
 
   console.log('Seed ejecutado correctamente');
 }
