51367db3deda66e99c3387ef9d9ca08dae3269f6
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-18 12:26:43 -0500

    Semilla: relación productos y categorías.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -12,9 +12,21 @@ async function main() {
   const { categories, products } = initialData;
   // Categorias
   const categoriesData = categories.map((name) => ({ name }));
+
   await prisma.category.createMany({
     data: categoriesData,
-  })
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  console.log(categoriesMap);
+  
+  // Productos
 
   console.log('Seed ejecutado correctamente');
 }
