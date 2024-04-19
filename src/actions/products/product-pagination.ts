3c1721f7127ec22c8907618039ca803f5eaf4347
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-18 23:03:20 -0500

    Paginación: determinal el total de páginas.

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -12,6 +12,7 @@ export const getPaginatedProductsWithImages = async ({ page = 1, take = 12 }: Pa
   if (page < 1) page = 1;
 
   try {
+    // 1. Obtener los productos
     const products = await prisma.product.findMany({
       take: take,
       skip: (page - 1) * take,
@@ -25,9 +26,13 @@ export const getPaginatedProductsWithImages = async ({ page = 1, take = 12 }: Pa
       }
     });
 
+    // 2. Obtener el total de páginas.
+    const totalCount = await prisma.product.count();
+    const totalPages = Math.ceil(totalCount / take);
+
     return {
-      currentPage: 1,
-      totalPages: 10,
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map((product) => ({
         ...product,
         images: product.ProductImage.map((image) => image.url),
