29445d67192e15a8160c2a92c875041ee6d5786a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-18 23:20:47 -0500

    Paginación de sólo HTML y CSS.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,5 @@
 import { getPaginatedProductsWithImages } from "@/actions";
-import { ProductGrid, Title } from "@/components";
+import { Pagination, ProductGrid, Title } from "@/components";
 import { redirect } from "next/navigation";
 
 interface Props {
@@ -24,6 +24,7 @@ export default async function HomePage({ searchParams }: Props) {
         className="mb-2"
       />
       <ProductGrid products={products} />
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
