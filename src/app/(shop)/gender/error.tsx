a1a3fc42add27fa8eac5aecc072aeabc2c750a0d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-22 14:30:19 -0500

    Pagina de error para cuando el usuario ingrese desde la barra de url, una categoría que no existe.

---
+++ src/app/(shop)/gender/error.tsx
@@ -0,0 +1,9 @@
+'use client' // Error components must be Client Components
+ 
+import { PageNotFound } from '@/components';
+ 
+export default function Error() {
+  return (
+    <PageNotFound />
+  )
+}
