6c87372bfe99c9533ccc4334caf8807a5000266d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-24 23:14:51 -0500

    Client Side: Cantidad.

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -10,9 +10,20 @@ interface Props {
 
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>();
+  const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState(false);
+
+  const addToCart = () => {
+    setPosted(true);
+    if (!size) return;
+    console.log({ size, quantity });
+  };
 
   return (
     <>
+      {posted && !size && (
+        <span className="mt-2 text-red-500 fade-in">Debe de seleccionar una talla*</span>
+      )}
       {/* Selector de tallas */}
       <SizeSelector
         selectedSize={size}
@@ -21,10 +32,18 @@ export const AddToCart = ({ product }: Props) => {
       />
 
       {/* Selector de cantidad */}
-      <QuantitySelector quantity={2} />
+      <QuantitySelector
+        quantity={quantity}
+        onQuantityChanged={setQuantity}
+      />
 
       {/* Button */}
-      <button className="btn-primary my-5">Agregar al carrito</button>
+      <button
+        className="btn-primary my-5"
+        onClick={addToCart}
+      >
+        Agregar al carrito
+      </button>
     </>
   )
 }
