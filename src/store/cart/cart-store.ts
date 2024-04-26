b48a2e02c6ce5edcd36aa8bfff7679e48e124c85
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-25 21:25:51 -0500

    Store: hacer persistente el carrito de compras en el local storage.

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,5 +1,6 @@
 import type { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface State {
   cart: CartProduct[];
@@ -9,29 +10,34 @@ interface State {
 }
 
 export const useCartStore = create<State>()(
-  (set, get) => ({
-    cart: [],
-    //Methods
-    addProductToCart: (product: CartProduct) => {
-      const { cart } = get();
+  persist(
+    (set, get) => ({
+      cart: [],
+      //Methods
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
 
-      // 1. Revisar si el producto existe en el carrito con la talla seleccionada
-      const productInCart = cart.some((item) => item.id === product.id && item.size === product.size);
+        // 1. Revisar si el producto existe en el carrito con la talla seleccionada
+        const productInCart = cart.some((item) => item.id === product.id && item.size === product.size);
 
-      if (!productInCart) {
-        set({ cart: [...cart, product] });
-        return;
-      }
-
-      // 2. Se que el producto existe por talla... tengo que incrementarlo
-      const updatedCartProducts = cart.map((item) => {
-        if (item.id === product.id && item.size === product.size) {
-          return { ...item, quantity: item.quantity + product.quantity };
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
         }
-        return item;
-      });
 
-      set({ cart: updatedCartProducts });
+        // 2. Se que el producto existe por talla... tengo que incrementarlo
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+          return item;
+        });
+
+        set({ cart: updatedCartProducts });
+      }
+    }),
+    {
+      name: 'shopping-cart',
     }
-  })
+  ),
 );
