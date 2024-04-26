9265afba77c397254658be8824dfc35b6f54e8f9
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-25 20:51:41 -0500

    Store: Agregar producto al carrito de compras.

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -2,7 +2,8 @@
 
 import { useState } from "react";
 import { QuantitySelector, SizeSelector } from "@/components"
-import { Product, Size } from "@/interfaces"
+import type { CartProduct, Product, Size } from "@/interfaces"
+import { useCartStore } from "@/store";
 
 interface Props {
   product: Product;
@@ -12,11 +13,25 @@ export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
   const [posted, setPosted] = useState(false);
+  const addProductToCart = useCartStore(state => state.addProductToCart);
 
   const addToCart = () => {
     setPosted(true);
     if (!size) return;
-    console.log({ size, quantity });
+
+    const cartProduct: CartProduct = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      price: product.price,
+      quantity: quantity,
+      size: size,
+      image: product.images[0]
+    };
+    addProductToCart(cartProduct);
+    setPosted(false);
+    setQuantity(1);
+    setSize(undefined);
   };
 
   return (
