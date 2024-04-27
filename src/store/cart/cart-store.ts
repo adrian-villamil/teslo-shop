ed53d2edbfddbf0ce2c83fd03b15428401852fd1
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-26 22:00:18 -0500

    Resumen de la orden.

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -5,6 +5,12 @@ import { persist } from "zustand/middleware";
 interface State {
   cart: CartProduct[];
   getTotalItems: () => number;
+  getSummaryInformation: () => {
+    subTotal: number;
+    tax: number;
+    total: number;
+    itemsInCart: number;
+  };
   addProductToCart: (product: CartProduct) => void;
   updateProductQuantity: (product: CartProduct, quantity: number) => void;
   removeProduct: (product: CartProduct) => void;
@@ -18,6 +24,16 @@ export const useCartStore = create<State>()(
       getTotalItems: () => {
         const { cart } = get();
         return cart.reduce((total, item) => total + item.quantity, 0);
+      },
+      getSummaryInformation: () => {
+        const { cart } = get();
+
+        const subTotal = cart.reduce((subTotal, product) => (product.price * product.quantity) + subTotal, 0);
+        const tax = subTotal * 0.15;
+        const total = subTotal + tax;
+        const itemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
+        return { subTotal, tax, total, itemsInCart };
       },
       addProductToCart: (product: CartProduct) => {
         const { cart } = get();
