6159582890259b2dec376a99d6fb3d9e5899ead9
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-29 22:37:50 -0500

    Cerrar sesión.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,3 +2,4 @@ export { authenticate } from './auth/login';
 export { getPaginatedProductsWithImages } from './products/product-pagination';
 export { getProductBySlug } from './products/get-product-by-slug';
 export { getStockBySlug } from './products/get-stock-by-slug';
+export { logout } from './auth/logout';
--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { useUIStore } from "@/store";
-import clsx from "clsx";
 import Link from "next/link";
+import clsx from "clsx";
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
+import { useUIStore } from "@/store";
+import { logout } from "@/actions";
 
 export const SideBar = () => {
   const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen);
@@ -75,13 +76,13 @@ export const SideBar = () => {
           <span className="ml-3 text-xl">Ingresar</span>
         </Link>
 
-        <Link
-          href={'/'}
-          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        <button
+          className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+          onClick={() => logout()}
         >
           <IoLogOutOutline size={30} />
           <span className="ml-3 text-xl">Salir</span>
-        </Link>
+        </button>
 
         {/* Line Separator */}
         <div className="w-full h-px bg-gray-200 my-10"></div>
