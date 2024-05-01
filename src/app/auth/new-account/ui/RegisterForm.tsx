4d6733be12729c290d34692468d8596351258886
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-04-30 20:12:27 -0500

    RegisterForm: mostrar errores en pantalla.

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from "clsx";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 
@@ -10,7 +11,7 @@ interface FormInputs {
 }
 
 export const RegisterForm = () => {
-  const { register, handleSubmit } = useForm<FormInputs>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>();
 
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     const { name, email, password } = data;
@@ -19,10 +20,16 @@ export const RegisterForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      {/* {errors.name?.type === 'required' && (
+        <span className="text-red-500">*El nombre es obligatorio</span>
+      )} */}
       <label htmlFor="name">Nombre completo</label>
       <input
         id="name"
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx(
+          "px-5 py-2 border bg-gray-200 rounded mb-5",
+          { 'border-red-500': errors.name }
+        )}
         type="text"
         autoFocus
         {...register('name', { required: true })}
@@ -31,7 +38,10 @@ export const RegisterForm = () => {
       <label htmlFor="email">Correo electrónico</label>
       <input
         id="email"
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx(
+          "px-5 py-2 border bg-gray-200 rounded mb-5",
+          { 'border-red-500': errors.email }
+        )}
         type="email"
         {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
       />
@@ -39,7 +49,10 @@ export const RegisterForm = () => {
       <label htmlFor="password">Contraseña</label>
       <input
         id="password"
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx(
+          "px-5 py-2 border bg-gray-200 rounded mb-5",
+          { 'border-red-500': errors.password }
+        )}
         type="password"
         {...register('password', { required: true })}
       />
