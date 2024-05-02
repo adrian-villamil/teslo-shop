87a7d90463d2754cd4c8af4842f6ea1ee70a8a42
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-01 22:30:13 -0500

    Mostrar los países en el formulario.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 export { authenticate, login } from './auth/login';
+export { getCountries } from './country/get-countries';
 export { getPaginatedProductsWithImages } from './products/product-pagination';
 export { getProductBySlug } from './products/get-product-by-slug';
 export { getStockBySlug } from './products/get-stock-by-slug';
