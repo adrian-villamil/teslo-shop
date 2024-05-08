6c1715cbbe0a5be4e9bfa092e97966e89e788ea6
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-08 16:53:55 -0500

    Guardar y cargar de zustand la dirección.

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { Country } from "@/interfaces";
+import { useAddressStore } from "@/store";
 import clsx from "clsx";
+import { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface FormInputs {
@@ -21,14 +23,25 @@ interface Props {
 }
 
 export const AddressForm = ({ countries }: Props) => {
-  const { handleSubmit, register, formState: { isValid } } = useForm<FormInputs>({
+  const { handleSubmit, register, formState: { isValid }, reset } = useForm<FormInputs>({
     defaultValues: {
       // Todo: leer de la base de datos
     }
   });
 
+  const setAddress = useAddressStore(state => state.setAddress);
+  const address = useAddressStore(state => state.address);
+
+  useEffect(() => {
+    if (address.firstName) {
+      reset(address);
+    }
+  }, [address.firstName]);
+
   const onSubmit: SubmitHandler<FormInputs> = (data) => {
     console.log({ data });
+
+    setAddress(data);
   };
 
   return (
