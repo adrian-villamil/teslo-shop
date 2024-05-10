bd070a888299faabb728ea164d7243b181c96c78
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-10 16:38:09 -0500

    Pantalla de confirmación: products del carrito.

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,13 +1,7 @@
 import { Title } from "@/components";
-import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import Link from "next/link";
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]
+import { ProducstInCart } from "./ui/ProducstInCart";
 
 export default function CheckoutPage() {
   return (
@@ -21,27 +15,7 @@ export default function CheckoutPage() {
             <Link href={'/cart'} className="underline mb-5">Editar carrito</Link>
 
             {/* Items */}
-            {productsInCart.map(product => (
-              <div key={product.slug} className="flex mb-5">
-                <Image
-                  src={`/products/${product.images[0]}`}
-                  width={100}
-                  height={100}
-                  style={{
-                    width: '100px',
-                    height: '100px',
-                  }}
-                  alt={product.title}
-                  className="mr-5 rounded-none"
-                />
-                <div>
-                  <p>{product.title}</p>
-                  <p>${product.price} x 3</p>
-                  <p className="font-bold">Subtotal: ${product.price*3}</p>
-                  <button className="underline mt-3">Remover</button>
-                </div>
-              </div>
-            ))}
+            <ProducstInCart />
           </div>
 
           {/* Checkout - Resumen de orden */}
