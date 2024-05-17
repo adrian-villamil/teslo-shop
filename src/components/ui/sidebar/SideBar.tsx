f059387494d625df9b2c7a10267c731139a6ff09
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-16 22:41:54 -0500

    Admin - Listado de productos.

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -102,8 +102,9 @@ export const SideBar = () => {
             <div className="w-full h-px bg-gray-200 my-10"></div>
             
             <Link
-              href={'/'}
+              href={'/admin/products'}
               className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+              onClick={closeMenu}
             >
               <IoShirtOutline size={30} />
               <span className="ml-3 text-xl">Productos</span>
